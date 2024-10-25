948f30f4a10a5f1ac6661533c4273f81ad51a2cc
jahidhasan6676 <jh18186676@gmail.com>
2024-10-25 12:26:17 +0600

    added the b9a7-solution all file

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Banner from './components/Banner/Banner'
+import Navbar from './components/Navbar/Navbar'
+function App() {
+
+
+  return (
+    <>
+      
+     <Navbar></Navbar>
+     <Banner></Banner>
+      
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import profile from '../../assets/logo.png';
+
+const Navbar = () => {
+    return (
+        <div className='flex justify-between items-center py-4 w-11/12 lg:w-10/12 mx-auto'>
+            {/* logo */}
+            <div>
+                <img className='w-16 h-16' src={profile}  />
+            </div>
+            {/* all li */}
+            <div>
+                <ul className='flex items-center gap-5 font-medium'>
+                    <li>Home</li>
+                    <li>Fixture</li>
+                    <li>Teams</li>
+                    <li>Schedule</li>
+                    <p className='flex items-center gap-1 border-2 p-2 rounded-lg font-medium'><span>0 </span>  coin <img className='w-6 h-6' src="https://img.icons8.com/?size=48&id=sPBQkuep9vDA&format=png"/></p>
+                </ul>
+
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
